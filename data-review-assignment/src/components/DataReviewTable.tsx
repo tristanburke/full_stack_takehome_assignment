9819009279a44e474c98c3eb4a1b3fdfb56671b6
Tristan Burke <tburke@hitbox.reviews>
2024-09-12 18:42:17 -0400

    update display, coloring, and initial tooltip pass

--- data-review-assignment/src/components/DataReviewTable.tsx
+++ data-review-assignment/src/components/DataReviewTable.tsx
@@ -1,6 +1,7 @@
 // components/DataReview.tsx
 
 import { useEffect, useState } from "react";
+import { Tooltip } from 'react-tooltip'
 
 export default function DataReviewTable() {
 
@@ -52,59 +53,87 @@ export default function DataReviewTable() {
         return <div>Loading...</div>;
     }
 
-    const cellFormatter = (errors: Errors, field: string) => {
-        return (
-            <td className="px-6 py-4">
-                {field}
-            </td>
-        )
+    const cellFormatter = (record: Record, field: keyof Record) => {
+        const errors = record.errors
+        if (field in errors) {
+            const getBackgroundColor = (severity: string) => {
+                if (severity === 'warning') return 'bg-yellow-400 bg-opacity-50';
+                if (severity === 'critical') return 'bg-red-500 bg-opacity-90';
+                return "";
+            }
+            const backgroundColor = getBackgroundColor(errors[field].severity);
+            const errorMessage = errors[field].message;
+
+            return (
+                // <td className={`px-6 py-4 text-gray-800 bg-opacity-50 ${backgroundColor}`}>
+                //     {record[field]}
+                // </td>
+                <td
+                    className={`px-6 py-4 text-gray-800 ${backgroundColor}`}
+                    data-tip={errorMessage} // Add error message for the tooltip
+                >
+                    {record[field]}
+                    <Tooltip place="top" /> {/* Initialize Tooltip */}
+                </td>
+            )
+        } else {
+            return (
+                <td className="px-6 py-4 text-gray-800 bg-green-600 bg-opacity-20">
+                    {record[field]}
+                </td>
+            )
+        }
     }
 
     return (
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <div className="flex justify-center items-center bg-gray-100">
-                <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
+        <div className="mx-auto">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <div className="flex justify-center items-center">
+                    <h1 className="text-3xl font-bold text-gray-700">Data Display!</h1>
+                </div>
+                <div className="py-6 flex items-center justify-center">
+                    <table className="shadow-md sm:rounded-lg w-2/3 text-sm text-left rtl:text-right text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr className="border-b-2 border-gray-800">
+                                <th scope="col" className="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Email
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Street
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    City
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Zipcode
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Phone
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Status
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((record) => (
+                                <tr className="border-b border-gray-400" key={record.id}>
+                                    {cellFormatter(record, "name")}
+                                    {cellFormatter(record, "email")}
+                                    {cellFormatter(record, "street")}
+                                    {cellFormatter(record, "city")}
+                                    {cellFormatter(record, "zipcode")}
+                                    {cellFormatter(record, "phone")}
+                                    {cellFormatter(record, "status")}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <table className="w-full text-sm text-left rtl:text-right text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Name
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Email
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Street
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            City
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Zipcode
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Phone
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Status
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((record) => (
-                        <tr key={record.id}>
-                            {cellFormatter(record.errors, record.name)}
-                            {cellFormatter(record.errors, record.email)}
-                            {cellFormatter(record.errors, record.street)}
-                            {cellFormatter(record.errors, record.city)}
-                            {cellFormatter(record.errors, record.zipcode)}
-                            {cellFormatter(record.errors, record.phone)}
-                            {cellFormatter(record.errors, record.status)}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
         </div>
 
     );
